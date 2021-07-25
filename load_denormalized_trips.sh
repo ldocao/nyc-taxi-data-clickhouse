3f2100327ab5ee5d5c4f44ff379989238f1a8844
Long Do Cao <long@homingbird.ai>
2021-07-25 21:52:44 +0200

    noise: echo filename

--- load_denormalized_trips.sh
+++ load_denormalized_trips.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 for filename in $HOME/nyc-taxi-data/denormalized/trips_x*.csv.gz; do
-            nohup gunzip -c $filename | \
-                python trans.py | \
-                clickhouse-client \
-                    --query="INSERT INTO denormalized_trips FORMAT CSV" --password=ZiVAAPCLkGJQCAkTUoV4P &
+    echo $filename
+    nohup gunzip -c $filename | \
+        python trans.py | \
+        clickhouse-client \
+            --query="INSERT INTO denormalized_trips FORMAT CSV" --password=ZiVAAPCLkGJQCAkTUoV4P &
 done
--- load_normalized_trips.sh
+++ load_normalized_trips.sh
@@ -1,6 +1,7 @@
 for filename in $HOME/nyc-taxi-data/normalized/trips_x*.csv.gz; do
-            nohup gunzip -c $filename | \
-                python trans.py | \
-                clickhouse-client \
-                    --query="INSERT INTO normalized_trips FORMAT CSV" --password=ZiVAAPCLkGJQCAkTUoV4P &
+    echo $filename
+    nohup gunzip -c $filename | \
+        python trans.py | \
+        clickhouse-client \
+            --query="INSERT INTO normalized_trips FORMAT CSV" --password=ZiVAAPCLkGJQCAkTUoV4P &
 done
