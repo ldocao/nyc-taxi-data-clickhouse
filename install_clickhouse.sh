f05e5526270dbcb0a79a6765ea1bcd4dee2201c0
Long Do Cao <long@homingbird.ai>
2021-08-05 15:35:54 +0200

    feat: create pk table

--- install_clickhouse.sh
+++ install_clickhouse.sh
@@ -15,4 +15,6 @@ clickhouse-client --multiquery --queries-file=denormalized_trips.sql --password=
 clickhouse-client --multiquery --queries-file=normalized_trips.sql --password=$PASSWORD
 clickhouse-client --multiquery --queries-file=normalized_weather.sql --password=$PASSWORD
 clickhouse-client --multiquery --queries-file=normalized_trips_mergetree.sql --password=$PASSWORD
+clickhouse-client --multiquery --queries-file=normalized_trips_mergetree_pk.sql --password=$PASSWORD
+
 sudo touch '/var/lib/clickhouse/flags/force_drop_table' && sudo chmod 666 '/var/lib/clickhouse/flags/force_drop_table' #enable drop of table greater than 50GB
