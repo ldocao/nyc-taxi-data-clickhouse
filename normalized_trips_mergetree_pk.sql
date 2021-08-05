b065b72c77fe2b8bd47797d023b9eedcc4bd55bb
Long Do Cao <long@homingbird.ai>
2021-08-05 15:27:20 +0200

    fix: engine pk

--- normalized_trips_mergetree_pk.sql
+++ normalized_trips_mergetree_pk.sql
@@ -2,9 +2,7 @@ DROP TABLE IF EXISTS normalized_trips_mergetree_pk;
 
 
 CREATE TABLE normalized_trips_mergetree_pk
-ENGINE = MergeTree(pickup_date, pickup_datetime, 8192) -- partition by, primary key, index_granularity, see deprecated method for creating table https://clickhouse.tech/docs/en/engines/table-engines/mergetree-family/mergetree/
-PRIMARY KEY trip_id
-AS SELECT
+AS SELECT (
     trip_id,
     CAST(vendor_id AS Enum8('1' = 1, '2' = 2, '3'= 3, '4' = 4, '5' = 5, 'CMT' = 6, 'VTS' = 7, 'DDS' = 8, 'B02512' = 10, 'B02598' = 11, 'B02617' = 12, 'B02682' = 13, 'B02764' = 14, '' = 15)) AS vendor_id,
     toDate(pickup_datetime) AS pickup_date,
@@ -29,4 +27,7 @@ AS SELECT
     assumeNotNull(total_amount) AS total_amount,
     CAST((assumeNotNull(payment_type) AS pt) IN ('CSH', 'CASH', 'Cash', 'CAS', 'Cas', '1') ? 'CSH' : (pt IN ('CRD', 'Credit', 'Cre', 'CRE', 'CREDIT', '2') ? 'CRE' : (pt IN ('NOC', 'No Charge', 'No', '3') ? 'NOC' : (pt IN ('DIS', 'Dispute', 'Dis', '4') ? 'DIS' : 'UNK'))) AS Enum8('CSH' = 1, 'CRE' = 2, 'UNK' = 0, 'NOC' = 3, 'DIS' = 4)) AS payment_type_,
     assumeNotNull(trip_type) AS trip_type
-FROM normalized_trips
+FROM normalized_trips )
+ENGINE = MergeTree(pickup_date, pickup_datetime, 8192) -- partition by, primary key, index_granularity, see deprecated method for creating table https://clickhouse.tech/docs/en/engines/table-engines/mergetree-family/mergetree/
+ORDER BY trip_id
+PRIMARY KEY trip_id
