fba5778d5b626d2b7e94b4c6b9bd2e20c253db0d
Long Do Cao <long@homingbird.ai>
2021-08-05 15:22:49 +0200

    feat: mergetree with pk

---
+++ normalized_trips_mergetree_pk.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS normalized_trips_mergetree_pk;
+
+
+CREATE TABLE normalized_trips_mergetree_pk
+ENGINE = MergeTree(pickup_date, pickup_datetime, 8192) -- partition by, primary key, index_granularity, see deprecated method for creating table https://clickhouse.tech/docs/en/engines/table-engines/mergetree-family/mergetree/
+PRIMARY KEY trip_id
+AS SELECT
+    trip_id,
+    CAST(vendor_id AS Enum8('1' = 1, '2' = 2, '3'= 3, '4' = 4, '5' = 5, 'CMT' = 6, 'VTS' = 7, 'DDS' = 8, 'B02512' = 10, 'B02598' = 11, 'B02617' = 12, 'B02682' = 13, 'B02764' = 14, '' = 15)) AS vendor_id,
+    toDate(pickup_datetime) AS pickup_date,
+    ifNull(pickup_datetime, toDateTime(0)) AS pickup_datetime,
+    toDate(dropoff_datetime) AS dropoff_date,
+    ifNull(dropoff_datetime, toDateTime(0)) AS dropoff_datetime,
+    assumeNotNull(store_and_fwd_flag) IN ('Y', '1', '2') AS store_and_fwd_flag,
+    assumeNotNull(rate_code_id) AS rate_code_id,
+    assumeNotNull(pickup_longitude) AS pickup_longitude,
+    assumeNotNull(pickup_latitude) AS pickup_latitude,
+    assumeNotNull(dropoff_longitude) AS dropoff_longitude,
+    assumeNotNull(dropoff_latitude) AS dropoff_latitude,
+    assumeNotNull(passenger_count) AS passenger_count,
+    assumeNotNull(trip_distance) AS trip_distance,
+    assumeNotNull(fare_amount) AS fare_amount,
+    assumeNotNull(extra) AS extra,
+    assumeNotNull(mta_tax) AS mta_tax,
+    assumeNotNull(tip_amount) AS tip_amount,
+    assumeNotNull(tolls_amount) AS tolls_amount,
+    assumeNotNull(ehail_fee) AS ehail_fee,
+    assumeNotNull(improvement_surcharge) AS improvement_surcharge,
+    assumeNotNull(total_amount) AS total_amount,
+    CAST((assumeNotNull(payment_type) AS pt) IN ('CSH', 'CASH', 'Cash', 'CAS', 'Cas', '1') ? 'CSH' : (pt IN ('CRD', 'Credit', 'Cre', 'CRE', 'CREDIT', '2') ? 'CRE' : (pt IN ('NOC', 'No Charge', 'No', '3') ? 'NOC' : (pt IN ('DIS', 'Dispute', 'Dis', '4') ? 'DIS' : 'UNK'))) AS Enum8('CSH' = 1, 'CRE' = 2, 'UNK' = 0, 'NOC' = 3, 'DIS' = 4)) AS payment_type_,
+    assumeNotNull(trip_type) AS trip_type
+FROM normalized_trips
